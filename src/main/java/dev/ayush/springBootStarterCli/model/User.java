e4bb1692c1bfe4d577a7409f04346984896ab608
ayush-devhub <ayush.devhub@gmail.com>
2025-09-10 16:10:15 +0530

    feat(model): add User entity

--- src/main/java/dev/ayush/springBootStarterCli/model/User.java
+++ src/main/java/dev/ayush/springBootStarterCli/model/User.java
@@ -1,4 +1,27 @@
 package dev.ayush.springBootStarterCli.model;
 
 public class User {
+    private final int id;
+    private final String email;
+
+    public User(int id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
